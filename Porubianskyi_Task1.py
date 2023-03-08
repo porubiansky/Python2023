9e01fc742c1d9a8a879e4ce6da036f66aea121ac
porubianskyi <valentyn.porubianskyi@infiz.khpi.edu.ua>
2023-03-08 20:42:51 +0200

    Lab1_first attempt

---
+++ Porubianskyi_Task1.py
@@ -0,0 +1,22 @@
+print('Мультипарадигмальні мови програмування. Лабороторна робота №1')
+print('Порубянський Валентин Валентинович, Група ІКМ-221д ')
+
+#use "if"
+name = 'Владислав Валерійович'
+count = 45
+long_name2 = ''
+for i in range(count):
+    if i < count-1:
+        long_name2 += name + ", "
+    else:
+        long_name2 += name
+print(long_name2)
+
+# use "*"
+name = 'Владислав Валерійович'
+long_name3 = (name + ', ') * 44 + name
+print(long_name3)
+
+# variant 12
+result = (176-9.3/7)/(18.2-179.3)-323.8**2
+print(round(result, 2))
